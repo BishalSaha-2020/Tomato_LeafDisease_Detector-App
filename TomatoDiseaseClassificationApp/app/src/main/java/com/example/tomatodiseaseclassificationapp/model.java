c1b2bf5ea52801afe173d693c5cbb3f41b0e3b39
Bishal <bishal.saha04052002@gmail.com>
2022-06-23 19:53:48 +0530

    Initial

---
+++ TomatoDiseaseClassificationApp/app/src/main/java/com/example/tomatodiseaseclassificationapp/model.java
@@ -0,0 +1,8 @@
+package com.example.tomatodiseaseclassificationapp;
+
+import android.content.Context;
+
+public class model {
+    public static model newInstance(Context applicationContext) {
+    }
+}
